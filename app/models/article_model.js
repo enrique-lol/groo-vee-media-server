c89c7935eeef0c901139c6d43c6c16fa200dfd51
enrique-lol <enrique.memije.pro@gmail.com>
2021-05-17 22:22:03 -0500

    Multi-Section Article New img (2-4) attribute to each article Header in each section (2-4) Paragraph for each section (2-4)

--- app/models/article_model.js
+++ app/models/article_model.js
@@ -7,9 +7,45 @@ const articleSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  content: {
+  intro: {
     type: String,
     required: true
+  },
+  img2: {
+    type: String,
+    required: false
+  },
+  heading2: {
+    type: String,
+    required: false
+  },
+  paragraph2: {
+    type: String,
+    required: false
+  },
+  img3: {
+    type: String,
+    required: false
+  },
+  heading3: {
+    type: String,
+    required: false
+  },
+  paragraph3: {
+    type: String,
+    required: false
+  },
+  img4: {
+    type: String,
+    required: false
+  },
+  heading4: {
+    type: String,
+    required: false
+  },
+  paragraph4: {
+    type: String,
+    required: false
   },
   authorName: {
     type: String,
@@ -31,7 +67,16 @@ const articleSchema = new mongoose.Schema({
       return {
         id: article._id,
         title: article.title,
-        content: article.content,
+        intro: article.intro,
+        img2: article.img2,
+        heading2: article.heading2,
+        paragraph2: article.paragraph2,
+        img3: article.img3,
+        heading3: article.heading3,
+        paragraph3: article.paragraph3,
+        img4: article.img4,
+        heading4: article.heading4,
+        paragraph4: article.paragraph4,
         authorName: article.authorName,
         mainImageUrl: article.mainImageUrl
       }
